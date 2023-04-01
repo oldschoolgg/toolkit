c36afb79d7acffbed973ecea8906053343d7937f
gc <30398469+gc@users.noreply.github.com>
2023-04-01 18:47:03 +1100

    add robochimp cl rank query util

--- src/util/misc.ts
+++ src/util/misc.ts
@@ -13,3 +13,13 @@ export function cleanString(str: string) {
 export function stringMatches(str: string | number = '', str2: string | number = '') {
 	return cleanString(str.toString()) === cleanString(str2.toString());
 }
+
+export function roboChimpCLRankQuery(userID: bigint) {
+	return `SELECT COUNT(*)
+FROM public.user
+WHERE ((osb_cl_percent + bso_cl_percent) / 2) >= (
+												  SELECT (((COALESCE(osb_cl_percent, 0)) + (COALESCE(bso_cl_percent, 0))) / 2)
+												  FROM public.user
+												  WHERE id = ${userID}
+												 );`;
+}
