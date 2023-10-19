f1d4c6f13d9da75347095309f15488db348aba1c
gc <30398469+gc@users.noreply.github.com>
2023-10-19 17:38:40 +1100

    Add enum

--- src/lib/Store.ts
+++ src/lib/Store.ts
@@ -1,43 +1,61 @@
-interface Product {
+export interface Product {
 	id: number;
 	name: string;
 	type: 'bit';
+	codePrefix: 'BG';
+}
+
+export enum ProductID {
+	SetOneNaturePermanentBankBackgrounds = 1,
+	SetTwoDarkPermanentBankBackgrounds = 2,
+	SetThreeTransparentAnimalsPermanentBankBackgrounds = 3,
+	SetFourGirlsPermanentBankBackgrounds = 4,
+	DynamicFarmingBackgroundPermanentBankBackgrounds = 5,
+	DynamicToBBackgroundPermanentBankBackgrounds = 6,
+	DynamicCoXBackgroundPermanentBankBackgrounds = 7
 }
 
 export const products: Product[] = [
 	{
-		id: 1,
+		id: ProductID.SetOneNaturePermanentBankBackgrounds,
 		name: 'Set 1 (Nature) Permanent Bank Background Unlock',
-		type: 'bit'
+		type: 'bit',
+		codePrefix: 'BG'
 	},
 	{
-		id: 2,
+		id: ProductID.SetTwoDarkPermanentBankBackgrounds,
 		name: 'Set 2 (Dark) Permanent Bank Background Unlock',
-		type: 'bit'
+		type: 'bit',
+		codePrefix: 'BG'
 	},
 	{
-		id: 3,
+		id: ProductID.SetThreeTransparentAnimalsPermanentBankBackgrounds,
 		name: 'Set 3 (Transparent Animals) Permanent Bank Background Unlock',
-		type: 'bit'
+		type: 'bit',
+		codePrefix: 'BG'
 	},
 	{
-		id: 4,
+		id: ProductID.SetFourGirlsPermanentBankBackgrounds,
 		name: 'Set 4 (Girls) Permanent Bank Background Unlock',
-		type: 'bit'
+		type: 'bit',
+		codePrefix: 'BG'
 	},
 	{
-		id: 5,
+		id: ProductID.DynamicFarmingBackgroundPermanentBankBackgrounds,
 		name: 'Dynamic Farming Background - Permanent Bank Background Unlock',
-		type: 'bit'
+		type: 'bit',
+		codePrefix: 'BG'
 	},
 	{
-		id: 6,
+		id: ProductID.DynamicToBBackgroundPermanentBankBackgrounds,
 		name: 'Dynamic ToB Background - Permanent Bank Background Unlock',
-		type: 'bit'
+		type: 'bit',
+		codePrefix: 'BG'
 	},
 	{
-		id: 7,
+		id: ProductID.DynamicCoXBackgroundPermanentBankBackgrounds,
 		name: 'Dynamic CoX Background - Permanent Bank Background Unlock',
-		type: 'bit'
+		type: 'bit',
+		codePrefix: 'BG'
 	}
 ];
