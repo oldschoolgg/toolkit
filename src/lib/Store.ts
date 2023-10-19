fff6e8733f1b097c2a7e68e5688c190af8eaba86
gc <30398469+gc@users.noreply.github.com>
2023-10-19 18:23:59 +1100

    Add bitfield enum

--- src/lib/Store.ts
+++ src/lib/Store.ts
@@ -1,9 +1,11 @@
-export interface Product {
+export type Product = {
 	id: number;
 	name: string;
-	type: 'bit';
 	codePrefix: 'BG';
-}
+} & {
+	type: 'bit';
+	bit: StoreBitfield;
+};
 
 export enum ProductID {
 	SetOneNaturePermanentBankBackgrounds = 1,
@@ -15,47 +17,64 @@ export enum ProductID {
 	DynamicCoXBackgroundPermanentBankBackgrounds = 7
 }
 
+export enum StoreBitfield {
+	HasSetOneNaturePermanentBankBackgrounds = 1,
+	HasSetTwoDarkPermanentBankBackgrounds = 2,
+	HasSetThreeTransparentAnimalsPermanentBankBackgrounds = 3,
+	HasSetFourGirlsPermanentBankBackgrounds = 4,
+	HasDynamicFarmingBackgroundPermanentBankBackgrounds = 5,
+	HasDynamicToBBackgroundPermanentBankBackgrounds = 6,
+	HasDynamicCoXBackgroundPermanentBankBackgrounds = 7
+}
+
 export const products: Product[] = [
 	{
 		id: ProductID.SetOneNaturePermanentBankBackgrounds,
 		name: 'Set 1 (Nature) Permanent Bank Background Unlock',
 		type: 'bit',
-		codePrefix: 'BG'
+		codePrefix: 'BG',
+		bit: StoreBitfield.HasSetOneNaturePermanentBankBackgrounds
 	},
 	{
 		id: ProductID.SetTwoDarkPermanentBankBackgrounds,
 		name: 'Set 2 (Dark) Permanent Bank Background Unlock',
 		type: 'bit',
-		codePrefix: 'BG'
+		codePrefix: 'BG',
+		bit: StoreBitfield.HasSetTwoDarkPermanentBankBackgrounds
 	},
 	{
 		id: ProductID.SetThreeTransparentAnimalsPermanentBankBackgrounds,
 		name: 'Set 3 (Transparent Animals) Permanent Bank Background Unlock',
 		type: 'bit',
-		codePrefix: 'BG'
+		codePrefix: 'BG',
+		bit: StoreBitfield.HasSetThreeTransparentAnimalsPermanentBankBackgrounds
 	},
 	{
 		id: ProductID.SetFourGirlsPermanentBankBackgrounds,
 		name: 'Set 4 (Girls) Permanent Bank Background Unlock',
 		type: 'bit',
-		codePrefix: 'BG'
+		codePrefix: 'BG',
+		bit: StoreBitfield.HasSetFourGirlsPermanentBankBackgrounds
 	},
 	{
 		id: ProductID.DynamicFarmingBackgroundPermanentBankBackgrounds,
 		name: 'Dynamic Farming Background - Permanent Bank Background Unlock',
 		type: 'bit',
-		codePrefix: 'BG'
+		codePrefix: 'BG',
+		bit: StoreBitfield.HasDynamicFarmingBackgroundPermanentBankBackgrounds
 	},
 	{
 		id: ProductID.DynamicToBBackgroundPermanentBankBackgrounds,
 		name: 'Dynamic ToB Background - Permanent Bank Background Unlock',
 		type: 'bit',
-		codePrefix: 'BG'
+		codePrefix: 'BG',
+		bit: StoreBitfield.HasDynamicToBBackgroundPermanentBankBackgrounds
 	},
 	{
 		id: ProductID.DynamicCoXBackgroundPermanentBankBackgrounds,
 		name: 'Dynamic CoX Background - Permanent Bank Background Unlock',
 		type: 'bit',
-		codePrefix: 'BG'
+		codePrefix: 'BG',
+		bit: StoreBitfield.HasDynamicCoXBackgroundPermanentBankBackgrounds
 	}
 ];
