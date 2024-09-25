41ddbb03170dd0e3ad708843dbc0feb8f360121e
gc <30398469+gc@users.noreply.github.com>
2024-09-26 03:24:18 +1000

    Export TSRedis

--- dist/structures.d.ts
+++ dist/structures.d.ts
@@ -2,4 +2,5 @@ export * from './lib/GeneralBank';
 export * from './lib/SimpleTable';
 export * from './lib/UserError';
 export * from './lib/Stopwatch';
+export * from './TSRedis';
 //# sourceMappingURL=structures.d.ts.map
