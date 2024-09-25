420da4c5866d25897c989155472ca8fc49cd480c
gc <30398469+gc@users.noreply.github.com>
2024-09-26 02:04:06 +1000

    Remove osjs stuff

--- dist/util/osjs.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Item } from 'oldschooljs/dist/meta/types';
-export declare function modifyItem(itemName: string | number, data: Partial<Item>): void;
-//# sourceMappingURL=osjs.d.ts.map
